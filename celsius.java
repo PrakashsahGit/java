f114f637c7a14fb5d88ae476fa3cf8f0a0912429
PrakashsahGit <96606084+PrakashsahGit@users.noreply.github.com>
2021-12-24 10:52:24 +0545

    Add files via upload

---
+++ celsius.java
@@ -0,0 +1,10 @@
+public class celsius{
+    static int fah(int n){
+        return(( 9/5*n) + 32);
+    }
+    public static void main(String[] args){
+        int result=fah(5);
+        System.out.println(result);
+    }
+}
+     
